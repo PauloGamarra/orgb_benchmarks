c3c254f482d6d513e1788e55c5d4b5da3d9b03ed
paulogamarra <paulogamarralessa@gmail.com>
2019-11-24 15:42:07 -0200

    benchmarks are now working on gem5 and gem5 tutorial was added

--- conv2d/tentativaconvolucao2.c
+++ conv2d/tentativaconvolucao2.c
@@ -5,6 +5,31 @@
 #define K_WIDTH1 3
 #define K_WIDTH2 3
 
+void convolution_2D(float I[I_WIDTH1][I_WIDTH2], float K[K_WIDTH1][K_WIDTH2], float O[I_WIDTH1][I_WIDTH2]) {
+
+    for (int i = 0; i < I_WIDTH1; ++i)
+    {
+        for (int j = 1; j < I_WIDTH2-1; ++j)
+        {
+            if(i == 0 || i == I_WIDTH1 - 1 || j == 0 || j == I_WIDTH2 - 1)
+            {
+                O[i][j] = 0;
+            }
+            else
+            {
+                for (int m = -1; m <= 1; m++)
+                {
+                    for (int n = -1; n <= 1; n++)
+                    {
+                        O[i][j] += I[i+m][j+n] * K[1-m][1-n];
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 int main(){
 
     for (int m = -1; m <= 1; m++)
@@ -33,31 +58,8 @@ int main(){
         printf("\n");
     }
 
-    return;
+    return 0;
 }
 
 
-void convolution_2D(float I[I_WIDTH1][I_WIDTH2], float K[K_WIDTH1][K_WIDTH2], float O[I_WIDTH1][I_WIDTH2]) {
-
-    for (int i = 0; i < I_WIDTH1; ++i)
-    {
-        for (int j = 1; j < I_WIDTH2-1; ++j)
-        {
-            if(i == 0 || i == I_WIDTH1 - 1 || j == 0 || j == I_WIDTH2 - 1)
-            {
-                O[i][j] = 0;
-            }
-            else
-            {
-                for (int m = -1; m <= 1; m++)
-                {
-                    for (int n = -1; n <= 1; n++)
-                    {
-                        O[i][j] += I[i+m][j+n] * K[1-m][1-n];
-                    }
-                }
-            }
-        }
-    }
-}
 
--- doubleIntegral/Untitled1.c
+++ doubleIntegral/Untitled1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 // C++ program to calculate
 // double integral value
 
